c4b18f13fa71111d90fd2c3c3bb3f799b3a58fa6
Jack-U-0018 <157313255+Jack-U-0018@users.noreply.github.com>
2025-01-21 17:04:01 -0500

    Add files via upload

---
+++ Jack_Upton_Module_1_Programming_Assignment___Numbers_and_Types.py
@@ -0,0 +1,28 @@
+print("Problem 3.1: ")
+print("How many seconds are there in an hour?")
+print("60 * 60")
+hour = (60 * 60)
+print("There are", hour, "seconds in an hour.")
+
+print("Problem 3.2: ")
+seconds_per_hour = hour
+
+print("Problem 3.3: ")
+print("How many seconds are in a day?")
+seconds = seconds_per_hour * 24
+print("There are", seconds,"in a day")
+
+print("Problem 3.4: ")
+print("How many seconds are in a day?")
+seconds_per_day = seconds_per_hour * 24
+print("There are", seconds_per_day,"in a day")
+
+print("Problem 3.5: ")
+print("What is the seconds per day divided by the seconds per hour?")
+answer = (seconds_per_day/seconds_per_hour)
+print("There are", answer,"divided by the seconds per hour")
+
+print("Problem 3.6: ")
+print("What is the seconds per day divided by the seconds per hour using intiger division?")
+time = (seconds_per_day // seconds_per_hour)
+print("The answer is", time, "seconds per day divided by the seconds per hour using intiger division")
